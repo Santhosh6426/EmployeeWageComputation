f0ef46089bab4f256309d5e82606b033819753cb
Santhosh V <santhosh.v6426@gmail.com>
2020-09-10 19:10:03 +0530

    Added:Added program to check Employee Attendance.

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,1 +1,10 @@
 echo "Welcome to Employee Wage Computation Program"
+#!/bin/bash -x
+IsPresent=1
+RandomCheck=$(( RANDOM%2 ))
+if [ $IsPresent -eq $RandomCheck ]
+then
+        echo "Employee is present"
+else
+        echo "Not present"
+fi
