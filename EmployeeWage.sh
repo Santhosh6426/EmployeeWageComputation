800e0dc8b5cc86be92be8b3565afec6b53fae929
Santhosh V <santhosh.v6426@gmail.com>
2020-09-10 22:30:27 +0530

    Added:Added the program to compute Monthlywage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,19 +1,26 @@
 echo "Welcome to Employee Wage Computation Program"
 #!/bin/bash
 HalfTimeHours=4
-DailyWage=0
 WagePerHour=20
 FullTimeHours=8
-RandomCheck=`echo $(($RANDOM%3))`
-case $RandomCheck in
-	"0")echo "Employee is absent" ;;
-	"1")
-	DailyWage=$(( $HalfTimeHours * $WagePerHour ))
-	echo "Employee is present for halfday" 
-	;;
-	"2")
-	DailyWage=$(( $FullTimeHours * $WagePerHour ))
-	echo "Employee is present for full day" 
-	;;
-esac
-echo "Daily wage of the employee is $DailyWage"
+WorkingDays=20
+MonthlyWage=0
+for (( day=1; day<=$WorkingDays; day++ ))
+do
+	RandomCheck=`echo $(($RANDOM%3))`
+	echo -e "\nDay: $day"
+	DailyWage=0
+	case $RandomCheck in
+		"0")echo "Employee is absent" ;;
+		"1")
+		DailyWage=$(( $HalfTimeHours * $WagePerHour ))
+		echo "Employee is present for halfday" 
+		;;
+		"2")
+		DailyWage=$(( $FullTimeHours * $WagePerHour ))
+		echo "Employee is present for full day" 
+		;;
+	esac
+	MonthlyWage=$(( $MonthlyWage + $DailyWage ))
+	echo -e "DailyWage: $DailyWage     MonthlyWage: $MonthlyWage"
+done
